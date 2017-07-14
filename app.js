6108e7753fa62dad9fae87878ce9611e9a237a2f
han8909227 <hbao2016@hotmail.com>
2017-07-14 13:32:25 -0700

    added mutiplication function

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ For TODO item, be sure to change it to say DONE when you have successfully compl
 
 // TODO: Write your code here
 function sum(a,b){ //eslint-disable-line
-  var reuslt = a + b;
+  var result = a + b;
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
 
-  return [message];
+  return [result,message];
 
 }
 
@@ -35,10 +35,15 @@ Test this function by hand in the console to get it working, and when you think 
 // TODO: Write your code here
 function multiply(a,b){ //eslint-disable-line
 
+  var result = a*b;
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + result + '.';
+
+  return [result,message];
+
 }
 
 // TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
